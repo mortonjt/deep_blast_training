e9094c74cea79594b64d0d968d8623c78b76d46f
mortonjt <jamietmorton@gmail.com>
2020-10-21 21:19:39 -0600

    uploading scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,19 +1,28 @@
 DIR=malisam
 seqfile=../data/structure_benchmarks/$DIR/seqs.fasta
-results_dir=../results/hmmer
-phmmer --max -A $results_dir/${DIR}_hmmer_alignments.txt -o $results_dir/$DIR-hmm.out $seqfile $seqfile
+
+# results_dir=../results/hmmer
+# phmmer --max -A $results_dir/${DIR}_hmmer_alignments.txt -o $results_dir/$DIR-hmm.out $seqfile $seqfile
 
 # results_dir=../results/blast/
 # makeblastdb -in $seqfile -dbtype 'prot' -out $results_dir/db
 # blastp -db $results_dir/db -query $seqfile -out $results_dir/${DIR}_blast_alignments.xml -outfmt 5
+
+results_dir=../results/psiblast/
+makeblastdb -in $seqfile -dbtype 'prot' -out $results_dir/db
+psiblast -db $results_dir/db -query $seqfile -out $results_dir/${DIR}_blast_alignments.xml -outfmt 5
+
+
 
 DIR=malidup
 seqfile=../data/structure_benchmarks/$DIR/seqs.fasta
-results_dir=../results/hmmer
-phmmer --max -A $results_dir/${DIR}_hmmer_alignments.txt -o $results_dir/$DIR-hmm.out $seqfile $seqfile
+# results_dir=../results/hmmer
+# phmmer --max -A $results_dir/${DIR}_hmmer_alignments.txt -o $results_dir/$DIR-hmm.out $seqfile $seqfile
 
 # results_dir=../results/blast/
 # makeblastdb -in $seqfile -dbtype 'prot' -out $results_dir/db
 # blastp -db $results_dir/db -query $seqfile -out $results_dir/${DIR}_blast_alignments.xml -outfmt 5
 
-MAMMOTH=/home/juermieboop/Documents/software/mastadon/mammoth
+results_dir=../results/psiblast/
+makeblastdb -in $seqfile -dbtype 'prot' -out $results_dir/db
+psiblast -db $results_dir/db -query $seqfile -out $results_dir/${DIR}_blast_alignments.xml -outfmt 5
